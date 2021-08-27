e7ce9076376c6ccbf9c11137f5b1640feb32ae8b
Devesh Bhangale1 <devesh.bhangale1@morningstar.com>
2021-08-28 02:16:53 +0530

    Program9, Extended Prime number program implementing 2D array by adding anagrams and non anagrams number

--- src/com/dsprograms/PrimeNumbers.java
+++ src/com/dsprograms/PrimeNumbers.java
@@ -20,20 +20,68 @@ public class PrimeNumbers<E> {
 		return primeArr;
 	}
 	
+	private static int reverse(int change) {
+		int res=0;
+		while(change!=0) {
+			int check = change%10;
+			res = res*10+check;
+			change /= 10;
+		}
+		return res;
+	}
+	
+	public static Integer[] anagrams(Integer[] newArr,int totalSize) {
+		Integer[] anagram = new Integer[totalSize];
+		int k =0;
+		for(int i=0;i<newArr.length-1;i++) {
+			for(int j=i+1;j<newArr.length;j++) {
+				if (newArr[j]!=null && newArr[i]!=null &&reverse(newArr[i])==newArr[j]) {
+					anagram[k]=newArr[i];
+					k++;
+					anagram[k]=reverse(newArr[i]);
+					k++;
+				}
+			}
+		}
+		return anagram;
+	}
+	
 	public static void main(String[] args) {
 		
 		Integer[][] primeNumbers = new Integer[10][30];
 		for(int i=0,j=0;i<1000;i=i+100,j++) {
 			primeNumbers[j]= prime(i+1,i+100);
 		}
-		
+		Integer[] newArr = new Integer[300];
+		int k=0;
 		for(int i=0;i<10;i++) {
 			for(int j=0;j<30;j++) {
-				if (primeNumbers[i][j]!=null) System.out.print(primeNumbers[i][j]+" ");
+				if (primeNumbers[i][j]!=null) {
+					newArr[k]=primeNumbers[i][j];
+					k++;
+				}
 			}
-			System.out.println();
 		}
 		
+		Integer[] anagrams = anagrams(newArr,k+1);
+		Integer[] notAnagrams = new Integer[k+1];
+		
+		int z=0;
+		for(int i=0;i<newArr.length;i++) {
+			int flag = 0;
+			for(int j=0;j<anagrams.length;j++) {
+				if (newArr[i]==anagrams[j]) {
+					flag = 1;
+					break;
+				}
+			}
+			if (flag == 0) {
+				notAnagrams[z]=newArr[i];
+				z++;
+			}
+		}
+		
+		Integer[][] anagramsNotAnagrams = new Integer[][] {anagrams,notAnagrams};
 	}
 	
 }
